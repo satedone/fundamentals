b144968a62ddcc97bef06bdd4f7f521d478b7bbc
satedone <nz@intop.dp.ua>
2023-08-29 23:47:56 +0300

    finished homework 4th lesson

--- 4. DEBUGGING. HANDLING EXCEPTIONS/homework.js
+++ 4. DEBUGGING. HANDLING EXCEPTIONS/homework.js
@@ -1,4 +1,6 @@
+// ************************************
 // ************** Task 1 **************
+// ************************************
 
 function hasNonNumericValue(arr) {
   for (let i = 0; i < arr.length; i++) {
@@ -33,39 +35,126 @@ console.log(sumSliceArray([1, 2, 3, 4, 5], 2, 4)); // 8
 console.log(sumSliceArray([1, "fff", 3, 4, 5], 2, 4)); // Array contains non-numeric values
 console.log(sumSliceArray([1, 2, 3, 4, 5], 2, 7)); // Indexes are out of range
 
-
+// ************************************
 // ************** Task 2 **************
-
+// ************************************
 
 function checkAge() {
-    try {
-      const name = prompt('Please enter your name:');
-      if (!name) {
-        throw new Error('The field is empty! Please enter your name');
-      }
-  
-      const age = parseInt(prompt('Please enter your age:'));
-      if (isNaN(age)) {
-        throw new EvalError('Age should be a number');
-      }
-  
-      if (age < 18 || age > 70) {
-        throw new RangeError('You must be between 18 and 70 years old to watch the movie');
-      }
-  
-      const status = prompt('Please enter your status (admin, moderator, user):');
-      if (status !== 'admin' && status !== 'moderator' && status !== 'user') {
-        throw new EvalError('Invalid status');
-      }
-  
-      alert('You have access to watch the movie!');
-    } catch (error) {
-      alert(`Error: ${error.name}\n${error.message}`);
+  try {
+    const name = prompt('Please enter your name:');
+    if (!name) {
+      throw new Error('The field is empty! Please enter your name');
     }
+
+    const age = parseInt(prompt('Please enter your age:'));
+    if (isNaN(age)) {
+      throw new EvalError('Age should be a number');
+    }
+
+    if (age < 18 || age > 70) {
+      throw new RangeError('You must be between 18 and 70 years old to watch the movie');
+    }
+
+    const status = prompt('Please enter your status (admin, moderator, user):');
+    if (status !== 'admin' && status !== 'moderator' && status !== 'user') {
+      throw new EvalError('Invalid status');
+    }
+
+    alert('You have access to watch the movie!');
+  } catch (error) {
+    alert(`Error: ${error.name}\n${error.message}`);
+  }
+}
+
+checkAge();
+
+// ************************************
+// ************** Task 3 **************
+// ************************************
+
+function calcRectangleArea(width, height) {
+  if (typeof width !== 'number' || typeof height !== 'number') {
+    throw new Error('Both width and height should be numbers');
+  }
+
+  return width * height;
+}
+
+try {
+  const widthInput = prompt('Enter the width of the rectangle:');
+  const heightInput = prompt('Enter the height of the rectangle:');
+
+  const width = parseFloat(widthInput);
+  const height = parseFloat(heightInput);
+
+  const area = calcRectangleArea(width, height);
+  alert(`The area of the rectangle is: ${area}`);
+} catch (error) {
+  alert(`Error: ${error.message}`);
+}
+
+// ************************************ 
+// ************** Task 4 **************
+// ************************************
+
+class MonthException extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'MonthException';
+  }
+}
+
+function showMonthName(month) {
+  const monthNames = [
+    'January', 'February', 'March', 'April',
+    'May', 'June', 'July', 'August',
+    'September', 'October', 'November', 'December'
+  ];
+
+  if (month < 1 || month > 12) {
+    throw new MonthException('Incorrect month number');
+  }
+
+  return monthNames[month - 1];
+}
+
+try {
+  console.log(showMonthName(5));  
+  console.log(showMonthName(14));
+} catch (error) {
+  console.log(`${error.name} ${error.message}`);
+}
+
+// ************************************
+// ************** Task 5 **************
+// ************************************
+
+function showUser(id) {
+    if (id < 0) {
+      throw new Error('ID must not be negative');
+    }
+  
+    return { id };
   }
   
-  checkAge();
+  function showUsers(ids) {
+    const validUsers = [];
   
-// ************** Task 3 **************
+    for (const id of ids) {
+      try {
+        const user = showUser(id);
+        validUsers.push(user);
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+  
+    return validUsers;
+  }
+  
+  const users = showUsers([7, -12, 44, 22]);
+  console.log(users);
+  
+
 
 
